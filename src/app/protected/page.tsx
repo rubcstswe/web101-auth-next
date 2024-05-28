962fcf98aa4e9ccdb862a696308d9fef7015c53d
douglasswm <douglas.cst@rub.edu.bt>
2024-05-28 16:43:58 +0600

    protected routes

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -3,11 +3,13 @@ import { auth } from "../../../auth";
 
 export default async function Protected() {
   const session = await auth();
-  if (!session?.user) redirect("/");
+  if (!session?.user) {
+    return <h1>You need to sign in to access this page</h1>;
+  }
   return (
     <div>
-      <p>JWT:{session.user.token ?? ""}</p>
-      <p>Email: {session.user.email ?? ""}</p>
+      <p>JWT:{session?.user.token ?? "YOU ARE NOT SIGNED IN"}</p>
+      <p>Email: {session?.user.email ?? "YOU ARE NOT SIGNED IN"}</p>
       <h1>YOU ARE ON A PROTECTED PAGE</h1>
     </div>
   );
